e3a72b7421bb6f4663460ba779d3a8e32b1773c8
AndrewKuo <andrewkuogs@gmail.com>
2017-10-05 14:41:24 +0800

    Upload

--- hw2/src/Calculator.java
+++ hw2/src/Calculator.java
@@ -3,12 +3,13 @@ import java.util.Stack;
 public class Calculator {
 
     private Double calculate(String s, double a, double b){
+
         switch (s){
-            case "+": { return b + a; }
-            case "-": { return b - a; }
-            case "*": { return b * a; }
-            case "/": { return b / a; }
-            default: { return 0.0; } // This should not happen.
+            case "+": return b + a;
+            case "-": return b - a;
+            case "*": return b * a;
+            case "/": return b / a;
+            default: StdOut.println(s); return -1000.0; // This should not happen.
         }
     }
 
@@ -24,19 +25,22 @@ public class Calculator {
                 switch (s){
                     case "+":
                     case "-":
-                    case "(": { op.push(s); }
+                    case "(":
+                        op.push(s);
+                        break;
                     case "*":
-                    case "/": {
+                    case "/":
                         op.push(s);
                         priority = true;
-                    }
-                    case ")": {
-                        while (!op.pop().equals("(")){
+                        break;
+                    case ")":
+                        if (!op.peek().equals("(")){
                             va.push(calculate(op.pop(), va.pop(), va.pop()));
                         }
-                    }
+                        break;
                     default:{
                         va.push(Double.parseDouble(s));
+                        break;
                     }
                 }
             }
@@ -44,17 +48,34 @@ public class Calculator {
             else{
                 priority = false;
                 switch (s){
-                    case "(":{ op.push(s); }
+                    case "(":{
+                        op.push(s);
+                        break;
+                    }
                     // default in here will only be values.
                     default:{
                         va.push(calculate(op.pop(), Double.parseDouble(s), va.pop()));
+                        break;
                     }
                 }
             }
         }
 
-        while(op.pop() != null){
-            va.push(calculate(op.pop(), va.pop(), va.pop()));
+        String front_sign, back_sign;
+        Double front_value, back_value;
+        while(va.size() > 1){
+            //if (op.peek().equals("(")){ op.pop(); }
+
+            back_sign = op.pop();
+            back_value = va.pop();
+            if (va.size() == 1){ front_sign = "+"; }
+            else{ front_sign = op.pop(); }
+            front_value = va.pop();
+
+            if (back_sign.equals("-")){ back_value *= -1; }
+            if (front_sign.equals("-")){ front_value *= -1; }
+            va.push(back_value + front_value);
+            op.push("+");
         }
 
         return va.pop();
--- hw2/src/Main.java
+++ hw2/src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Calculator cct = new Calculator();
+        Double d = cct.ans("1 + 2 * ( 3 - 4 * 5 ) ");
+        StdOut.println(d);
+    }
 }
