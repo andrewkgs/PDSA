2669f4a34404ab2ea19f5939b5b63a48f7810eb8
AndrewKuo <andrewkuogs@gmail.com>
2017-10-05 09:01:38 +0800

    Upload

--- hw2/src/Calculator.java
+++ hw2/src/Calculator.java
@@ -1,9 +1,65 @@
+import java.util.Stack;
+
 public class Calculator {
 
-    public
+    private Double calculate(String s, double a, double b){
+        switch (s){
+            case "+": { return b + a; }
+            case "-": { return b - a; }
+            case "*": { return b * a; }
+            case "/": { return b / a; }
+            default: { return 0.0; } // This should not happen.
+        }
+    }
 
     public Double ans (String e) {
-    // please replace the following null to the answer you calculate.
-        return null;
+
+        Stack<Double> va = new Stack<>();
+        Stack<String> op = new Stack<>();
+        boolean priority = false; // if the previous operator is "*" or "/", then priority == true.
+
+        String[] content = e.split(" ");
+        for (String s: content){
+            if (!priority) {
+                switch (s){
+                    case "+":
+                    case "-":
+                    case "(": { op.push(s); }
+                    case "*":
+                    case "/": {
+                        op.push(s);
+                        priority = true;
+                    }
+
+                    case ")": {
+                        while (!op.pop().equals("(")){
+                            va.push(calculate(op.pop(), va.pop(), va.pop()));
+                        }
+                    }
+                    default:{
+                        va.push(Double.parseDouble(s));
+                    }
+                }
+            }
+
+            else{
+                priority = false;
+                switch (s){
+                    case "(":{
+                        op.push(s);
+                    }
+                    // default in here will only be values.
+                    default:{
+                        va.push(calculate(op.pop(), Double.parseDouble(s), va.pop()));
+                    }
+                }
+            }
+        }
+
+        while(op.pop() != null){
+            va.push(calculate(op.pop(), va.pop(), va.pop()));
+        }
+
+        return va.pop();
     }
 }
