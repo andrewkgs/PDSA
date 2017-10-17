3f972e721acf6aab4e63fd1ad8067e2defc1a37e
AndrewKuo <andrewkuogs@gmail.com>
2017-10-17 12:07:24 +0800

    Upload

---
+++ hw4/src/Onion.java
@@ -0,0 +1,12 @@
+public class Onion {
+    public static int[] ConvexHullVertex(Point2D[] a) {
+        // return the index set of the ConvexHullVertex,
+        // the index is the same as the index in array a:0, 1, 2, 3, 4, ....a.length-1
+    }
+
+    public static void main(String[] args) {
+        // 1. read in the file containing N 2-dimentional points
+        // 2. recursively find the convexhull of the resting points
+        // 3. count the number of convexhulls and print it
+    }
+}
